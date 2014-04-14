b00feb03f731055dbebc63343835f16e67abab1e
DavidNovo <david.novogrodsky@gmail.com>
2014-04-13 20:55:33 -0500

    tolopolgy change
    now otology runs on my MacOS install of Storm…happy!!

--- src/main/java/net/novogrodsky/PrimeNumberTopology.java
+++ src/main/java/net/novogrodsky/PrimeNumberTopology.java
@@ -19,39 +19,8 @@ public class PrimeNumberTopology {
 
 
         Config conf = new Config();
-/*
 
-// making sure workers are deployed to cluster
-        // remove these lines to run on command line
-        // java -jar storm-test-1.0-SNAPSHOT.jar from the target directory.
-        conf.setNumWorkers(2);
-        conf.setMaxSpoutPending(5000);
 
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("test", conf, builder.createTopology());
-
-        // this is used to stop the toology when run locally
-        // remove when deployed to a cluster
-        Utils.sleep(10000);
-        cluster.killTopology("test");
-        cluster.shutdown();
-*/
- /*       if (args != null && args.length > 0) {
-            conf.setNumWorkers(3);
-
-            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
-        }
-        else {
-            conf.setMaxTaskParallelism(3);
-
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("test", conf, builder.createTopology());
-
-            Thread.sleep(10000);
-
-            cluster.shutdown();
-        }
-*/
 
         conf.setDebug(true);
         conf.setNumWorkers(2);
